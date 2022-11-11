ce938b3d36242aa817745dad1efc0750e6246cc0
Nidea <todosesolucionara@gmail.com>
2022-11-11 14:50:03 +0100

    Update ncdu_setup.sh

--- Varios/ncdu_setup.sh
+++ Varios/ncdu_setup.sh
@@ -1,7 +1,7 @@
 # NCDU
 # · Utilidad para CLI, que permite ver de una forma muy visual que directorio está ocupando más
 #-----------------------------------------------------
-# URL						♦ https://dev.yorhel.nl/ncdu
+# [URL](https://dev.yorhel.nl/ncdu)
 
 # Descaraar y descomprimir
 wget https://dev.yorhel.nl/download/ncdu-2.2.1-linux-x86_64.tar.gz
